ebf82bbebb57ab3b11f302d0e6abe738b1337022
gavin9595 <wenjial@mtu.edu>
2022-03-05 11:44:00 -0500

    destrcutor added

--- learnconstructor.cpp
+++ learnconstructor.cpp
@@ -5,6 +5,7 @@ class circle{
 
     public:
     circle(double rad);
+    ~circle();
     double area();
     private:
     double radius;
@@ -12,9 +13,14 @@ class circle{
 
 circle::circle(double rad)
 {
+   cout<<"object created" <<endl;
    radius = rad;
 }
-
+circle::~circle()
+{
+   cout<<"object deleted" <<endl;
+   radius = 0;
+}
 double circle::area()
 {
     return 3.14*radius*radius;
