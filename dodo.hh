9ec4fc9bcca44e30ff037487735efb8a47a4234a
Claire <claire@ClairedeMacBook-Pro.local>
2022-11-28 13:20:50 -0500

    start learning cpp

---
+++ dodo.hh
@@ -0,0 +1,22 @@
+#pragma once
+#include "dio_states.hh"
+
+namespace novix
+{
+namespace embedded
+{
+namespace hwab
+{
+namespace io
+{
+
+
+    
+}
+
+}
+
+
+}
+
+}
---
+++ hwio.cpp
@@ -0,0 +1,24 @@
+#include "dio.hh"
+
+extern "C"
+{
+    #include <Dio.h>
+}
+
+namespace novix
+{
+namespace embedded
+{
+namespace hwab
+{
+namespace io
+{
+IoHwAbDioDo::IoHwAbDioDo(const std::uint_16& dio_channel):dio_channel_(dio_channel)
+
+}
+
+}
+
+}
+
+}
